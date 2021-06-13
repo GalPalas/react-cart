951759c8fdd471fbb1e201be35ce77078b98ed6e
Gal <galpalas89@gmail.com>
2021-06-13 13:56:46 +0300

    add redux cart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { fetchProducts } from "./store/products";
 import { useDispatch } from "react-redux";
 import Products from "./components/products";
@@ -9,11 +9,6 @@ import "./App.css";
 
 function App() {
   const dispatch = useDispatch();
-  const [cartItems, setCartItems] = useState(
-    localStorage.getItem("cartItems")
-      ? JSON.parse(localStorage.getItem("cartItems"))
-      : []
-  );
 
   useEffect(() => {
     dispatch(fetchProducts(data.products));
@@ -21,31 +16,6 @@ function App() {
 
   const createOrder = (order) => {
     alert("Need to save order for " + order.name);
-  };
-
-  const removeFromCart = (product) => {
-    const localCartItems = [...cartItems];
-    setCartItems(localCartItems.filter((item) => item._id !== product._id));
-    localStorage.setItem(
-      "cartItems",
-      JSON.stringify(localCartItems.filter((item) => item._id !== product._id))
-    );
-  };
-
-  const addToCart = (product) => {
-    const localCartItems = [...cartItems];
-    let alreadyInCart = false;
-    localCartItems.forEach((item) => {
-      if (item._id === product._id) {
-        item.count++;
-        alreadyInCart = true;
-      }
-    });
-    if (!alreadyInCart) {
-      localCartItems.push({ ...product, count: 1 });
-    }
-    setCartItems(localCartItems);
-    localStorage.setItem("cartItems", JSON.stringify(localCartItems));
   };
 
   return (
@@ -57,14 +27,10 @@ function App() {
         <div className="content">
           <div className="main">
             <Filter />
-            <Products addToCart={addToCart} />
+            <Products />
           </div>
           <div className="sidebar">
-            <Cart
-              cartItems={cartItems}
-              removeFromCart={removeFromCart}
-              createOrder={createOrder}
-            />
+            <Cart createOrder={createOrder} />
           </div>
         </div>
       </main>
