0a131366ef4b68659df5b4031e6ce602e6364d2c
Gal <galpalas89@gmail.com>
2021-06-13 14:31:16 +0300

    fix bug in modal product component

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import formatCurrency from "../utils";
 import Fade from "react-reveal/Fade";
 import Zoom from "react-reveal/Zoom";
@@ -10,14 +10,21 @@ import { addToCart } from "../store/cart";
 function Products() {
   const dispatch = useDispatch();
   const filterProducts = useSelector(getFilterdProducts());
+  const [product, setProduct] = useState(null);
 
-  // const openModal = (product) => {
-  //   setProduct(product);
-  // };
+  const openModal = (product) => {
+    setProduct(product);
+  };
 
-  // const closeModal = () => {
-  //   setProduct(null);
-  // };
+  const closeModal = () => {
+    setProduct(null);
+  };
+
+  const addItemFromModal = (product) => {
+    dispatch(addToCart({ product }));
+    closeModal();
+  };
+
   return !filterProducts ? (
     <div>Loading...</div>
   ) : (
@@ -27,11 +34,7 @@ function Products() {
           {filterProducts.map((product) => (
             <li key={product._id}>
               <div className="product">
-                <a
-                  href={
-                    "#" + product._id
-                  } /*onClick={() => openModal(product)}*/
-                >
+                <a href={"#" + product._id} onClick={() => openModal(product)}>
                   <img src={product.image} alt={product.title}></img>
                   <p>{product.title}</p>
                 </a>
@@ -50,79 +53,46 @@ function Products() {
           ))}
         </ul>
       </Fade>
+      {product && (
+        <Modal isOpen={true} onRequestClose={closeModal} ariaHideApp={false}>
+          <Zoom>
+            <button className="close-modal" onClick={closeModal}>
+              X
+            </button>
+            <div className="product-details">
+              <img src={product.image} alt={product.title}></img>
+              <div className="product-details-description">
+                <p>
+                  <strong>{product.title}</strong>
+                </p>
+                <p>{product.description}</p>
+                <p>
+                  available Sizes:{" "}
+                  {product.availableSizes.map((size) => (
+                    <span>
+                      {" "}
+                      <button className="button">{size}</button>
+                    </span>
+                  ))}
+                </p>
+                <div className="product-price">
+                  <div>{formatCurrency(product.price)}</div>
+                  <button
+                    className="button primary"
+                    onClick={() => {
+                      addItemFromModal(product);
+                    }}
+                  >
+                    Add To Cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Zoom>
+        </Modal>
+      )}
     </div>
   );
-  // return (
-  //   <div>
-  //     <Fade bottom cascade>
-  //       <ul className="products">
-  //         {products.map((product) => (
-  //           <li key={product._id}>
-  //             <div className="product">
-  //               <a
-  //                 href={
-  //                   "#" + product._id
-  //                 } /*onClick={() => openModal(product)}*/
-  //               >
-  //                 <img src={product.image} alt={product.title}></img>
-  //                 <p>{product.title}</p>
-  //               </a>
-
-  //               <div className="product-price">
-  //                 <div>{formatCurrency(product.price)}</div>
-  //                 <button
-  //                   onClick={() => addToCart(product)}
-  //                   className="button primary"
-  //                 >
-  //                   Add to cart
-  //                 </button>
-  //               </div>
-  //             </div>
-  //           </li>
-  //         ))}
-  //       </ul>
-  //     </Fade>
-  //     {/* {product && (
-  //       <Modal isOpen={true} onRequestClose={closeModal}>
-  //         <Zoom>
-  //           <button className="close-modal" onClick={closeModal}>
-  //             X
-  //           </button>
-  //           <div className="product-details">
-  //             <img src={product.image} alt={product.title}></img>
-  //             <div className="product-details-description">
-  //               <p>
-  //                 <strong>{product.title}</strong>
-  //               </p>
-  //               <p>{product.description}</p>
-  //               <p>
-  //                 available Sizes:{" "}
-  //                 {product.availableSizes.map((size) => (
-  //                   <span>
-  //                     {" "}
-  //                     <button className="button">{size}</button>
-  //                   </span>
-  //                 ))}
-  //               </p>
-  //               <div className="product-price">
-  //                 <div>{formatCurrency(product.price)}</div>
-  //                 <button
-  //                   className="button primary"
-  //                   onClick={() => {
-  //                     addToCart(product);
-  //                     closeModal();
-  //                   }}
-  //                 >
-  //                   Add To Cart
-  //                 </button>
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </Zoom>
-  //       </Modal>
-  //     )} */}
-  //   </div>
-  // );
 }
 
 export default Products;
